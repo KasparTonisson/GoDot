c9bedcb77f15989e4a8cd5581c59f564d0e61a41
Kaspar Tõnisson <98739379+KasparTonisson@users.noreply.github.com>
2022-04-07 11:32:14 +0300

    Add files via upload

---
+++ Ulesanne03.gd
@@ -0,0 +1,23 @@
+extends Node
+
+var elud = 100
+var vastane = 100
+
+
+func _ready():
+	
+	while vastane >= 0 or elud >= 0:
+		randomize()
+		var dmg = round(rand_range(8,15))
+		randomize()
+		var damage = round(rand_range(10,17))
+		print('P1 Hit: ',dmg,' P2 elud: ',vastane)
+		print('P2 Hit: ',damage,' P1 elud: ',elud)
+		vastane -= dmg
+		elud -= damage
+	print()
+	if vastane > elud:
+		print('Mängija 1 võitis!')
+	else:
+		print('Mängija 2 võitis!')
+
